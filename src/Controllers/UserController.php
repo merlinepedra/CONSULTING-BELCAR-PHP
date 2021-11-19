5bfc3a44397a04171b6e0fe3c4d7d5f765b94f31
Sol <solcis.bio@gmail.com>
2021-11-19 11:19:54 +0100

    change line 30 from userlist

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -3,7 +3,7 @@ require_once __DIR__ .'/../Models/User.php';
 
 class UserController {
     public function index(){
-        $user = (new User())->all();
+        $users = (new User())->all();
         require_once __DIR__ .'/../Views/Pages/userList.php'; 
     }
     public function create(){
