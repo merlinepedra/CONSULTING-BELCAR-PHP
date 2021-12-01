d2ebb9d09ded1dc35247bf2098bc945cee5d8a31
mcc72 <92302152+mcc72@users.noreply.github.com>
2021-12-01 12:02:41 +0100

    style header

--- src/Views/Components/header.php
+++ src/Views/Components/header.php
@@ -1,5 +1,5 @@
 <header>
-<nav class="navbar navbar-expand navbar-light">
+<nav class="navbar navbar-expand navbar-light d-flex justify-content-end ">
           <h1 class="title">THE CODER'S OFFICE</h1>
           <img class="logo" src="./../../../image/logo.png" alt="">
       </nav>   
--- src/Views/Pages/userList.php
+++ src/Views/Pages/userList.php
@@ -3,13 +3,13 @@
 <?php require_once __DIR__ .'/../Layouts/Default.php';?>
 
 <body>
-<img class="bg-img" src="../../../image/backimage.jpg" alt="...">
+
     
     <section class='row justify-content-center'>
         
     <div class="card">
     <div class="card-header">
-
+        <h2 class="d-flex justify-content-center" style="color:black">You are welcome!</h2>
         <a href="/users/create" name="" id="" class="btn btn-primary" role="button">Schedule an appointment</a>
 
     </div>
